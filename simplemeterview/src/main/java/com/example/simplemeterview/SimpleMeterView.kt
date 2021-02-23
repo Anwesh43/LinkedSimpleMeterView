52e557741964a4d56d441dc6b6f90837dc9ca25d
Anwesh43 <anweshthecool0@gmail.com>
2021-02-23 10:13:04 +0530

    adding SimpleMeter class

--- simplemeterview/src/main/java/com/example/simplemeterview/SimpleMeterView.kt
+++ simplemeterview/src/main/java/com/example/simplemeterview/SimpleMeterView.kt
@@ -159,4 +159,27 @@ class SimpleMeterView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class SimpleMeter(var i : Int, val state : State = State()) {
+
+        private var curr : SMNode = SMNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
