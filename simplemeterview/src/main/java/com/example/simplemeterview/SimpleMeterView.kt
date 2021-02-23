6690355c4c434a420dc4c7378d6fecfa115cfb52
Anwesh43 <anweshthecool0@gmail.com>
2021-02-23 10:10:24 +0530

    adding SMNode class

--- simplemeterview/src/main/java/com/example/simplemeterview/SimpleMeterView.kt
+++ simplemeterview/src/main/java/com/example/simplemeterview/SimpleMeterView.kt
@@ -118,4 +118,45 @@ class SimpleMeterView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SMNode(var i : Int, val state : State = State()) {
+
+        private var next : SMNode? = null
+        private var prev : SMNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SMNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSMNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SMNode {
+            var curr : SMNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
