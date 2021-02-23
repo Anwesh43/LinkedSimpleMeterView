cc053e43b0ac5e1771a33297509a1d140bafa10b
Anwesh43 <anweshthecool0@gmail.com>
2021-02-23 10:35:38 +0530

    adding fullscreen support

--- app/src/main/java/com/example/linkedsimplemeterview/MainActivity.kt
+++ app/src/main/java/com/example/linkedsimplemeterview/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.linkedsimplemeterview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.simplemeterview.SimpleMeterView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        SimpleMeterView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
