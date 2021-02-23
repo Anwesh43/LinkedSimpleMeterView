bb77d0d8fd673f379ae45d33b32d7fc19751223d
Anwesh43 <anweshthecool0@gmail.com>
2021-02-23 10:03:39 +0530

    adding Canvas extension methods

--- simplemeterview/src/main/java/com/example/simplemeterview/SimpleMeterView.kt
+++ simplemeterview/src/main/java/com/example/simplemeterview/SimpleMeterView.kt
@@ -29,3 +29,28 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawSimpleMeter(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    paint.style = Paint.Style.STROKE
+    save()
+    translate(w / 2, h / 2)
+    save()
+    rotate(start + deg * sf2)
+    drawLine(0f, 0f, size * sf1, 0f, paint)
+    restore()
+    drawArc(RectF(-size, -size, size, size), start, deg * sf2, false, paint)
+    restore()
+}
+
+fun Canvas.drawSMNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawSimpleMeter(scale, w, h, paint)
+}
