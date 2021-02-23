0b3d530cd8e04db5909bf71f1f8f5a2e214cbb11
Anwesh43 <anweshthecool0@gmail.com>
2021-02-23 10:14:50 +0530

    adding Renderer class

--- simplemeterview/src/main/java/com/example/simplemeterview/SimpleMeterView.kt
+++ simplemeterview/src/main/java/com/example/simplemeterview/SimpleMeterView.kt
@@ -182,4 +182,26 @@ class SimpleMeterView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : SimpleMeterView) {
+
+        private val animator : Animator = Animator(view)
+        private val sm : SimpleMeter = SimpleMeter(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            sm.draw(canvas, paint)
+            animator.animate {
+                sm.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            sm.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
